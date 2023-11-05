842cbb2037882f7948c3797c83803dcf4929d109
kenenbek <kenenbek@gmail.com>
2023-11-05 18:48:42 +0600

    f

--- mymodels.py
+++ mymodels.py
@@ -12,37 +12,37 @@ class AttnGCN(torch.nn.Module):
         super().__init__()
         torch.manual_seed(1234)
         self.conv1 = MYGATv2Conv(in_channels=6,
-                                 out_channels=256,
+                                 out_channels=64,
                                  heads=2,
                                  edge_dim=6,
                                  aggr="add",
                                  concat=True,
                                  share_weights=False)
-        self.norm1 = BatchNorm1d(512)
+        self.norm1 = BatchNorm1d(128)
 
-        self.conv2 = MYGATv2Conv(in_channels=512,
-                                 out_channels=256,
+        self.conv2 = MYGATv2Conv(in_channels=128,
+                                 out_channels=64,
                                  heads=2,
                                  edge_dim=6,
                                  aggr="add",
                                  concat=True,
                                  share_weights=False)
-        self.norm2 = BatchNorm1d(512)
-        self.conv3 = MYGATv2Conv(in_channels=512,
-                                 out_channels=256,
+        self.norm2 = BatchNorm1d(128)
+        self.conv3 = MYGATv2Conv(in_channels=128,
+                                 out_channels=64,
                                  heads=2,
                                  edge_dim=6,
                                  aggr="add",
                                  concat=True,
                                  share_weights=False)
-        self.norm3 = BatchNorm1d(512)
-        self.fc1 = Linear(512, 512)
-        self.fc_norm1 = BatchNorm1d(512)
-        self.fc2 = Linear(512, 512)
-        self.fc_norm2 = BatchNorm1d(512)
-        self.fc3 = Linear(512, 5)
+        self.norm3 = BatchNorm1d(64)
+        self.fc1 = Linear(64, 64)
+        self.fc_norm1 = BatchNorm1d(64)
+        self.fc2 = Linear(64, 64)
+        self.fc_norm2 = BatchNorm1d(64)
+        self.fc3 = Linear(64, 5)
 
-        self.dp = 0.2
+        self.dp = 0.1
 
     def forward(self, h, edge_index, edge_weight):
         h = self.norm1(self.conv1(h, edge_index, edge_weight)).relu()
