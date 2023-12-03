6ad103c2051c0af136a5bbc7991c8dda44b72177
kenenbek <kenenbek@gmail.com>
2023-12-03 19:17:27 +0600

    f

--- utils.py
+++ utils.py
@@ -223,7 +223,7 @@ def create_25_graphs(y, train_edge_index, train_edge_weight, q=0.1):
     indices_to_change = torch.randperm(len(y))[:num_labels_to_change].to(device)
 
     # Assign random labels to these indices
-    y[indices_to_change] = torch.randint(0, 5, (num_labels_to_change,))
+    y[indices_to_change] = torch.randint(0, 5, (num_labels_to_change,)).to(device)
 
     sub_data_s = []
     for src in range(5):
