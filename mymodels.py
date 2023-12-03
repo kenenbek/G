94f9b3a1c21b272119c8faac01100bf2161d5feb
kenenbek <kenenbek@gmail.com>
2023-12-03 21:33:43 +0600

    f

--- mymodels.py
+++ mymodels.py
@@ -14,7 +14,7 @@ class BigAttn(torch.nn.Module):
         super().__init__()
 
         n_sub_graphs = 25
-        feature_dim = 64
+        feature_dim = 128
 
         self.conv_layers_1a = torch.nn.ModuleList([])
         self.batch_norms_1a = torch.nn.ModuleList([])
