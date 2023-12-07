573890881e215bd873640f288d62e41842536fcc
kenenbek <kenenbek@gmail.com>
2023-12-08 03:01:16 +0600

    f

--- mymodels.py
+++ mymodels.py
@@ -138,7 +138,7 @@ class AttnGCN(torch.nn.Module):
                                edge_dim=1,
                                aggr="mean",
                                concat=False,
-                               share_weights=True,
+                               share_weights=False,
                                add_self_loops=True)
         self.norm1 = BatchNorm1d(n_features)
 
