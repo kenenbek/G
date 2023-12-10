4a7b1a2944ab3f549c5e88b5319d9a10a2d835ca
kenenbek <kenenbek@gmail.com>
2023-12-10 23:24:32 +0600

    f

--- mymodels.py
+++ mymodels.py
@@ -163,19 +163,10 @@ class AttnGCN(torch.nn.Module):
 class SimpleNN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.dp = 0.2
-        self.norm0 = BatchNorm1d(15)
-        self.fc1 = Linear(15, 128)
-        self.norm1 = BatchNorm1d(128)
-        self.fc2 = Linear(128, 128)
-        self.norm2 = BatchNorm1d(128)
-        self.fc3 = Linear(128, 5)
+        self.fc1 = Linear(15, 5)
 
     def forward(self, x_input, bf, graph25, edge_index, edge_weight):
-        h = self.norm0(bf)
-        h = self.norm1(self.fc1(h)).relu()
-        h = self.norm2(self.fc2(h)).relu()
-        h = self.fc3(h)
+        h = self.fc1(bf)
         return h
 
 
