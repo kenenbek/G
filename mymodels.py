473a7e24d931e71ea02c6a9d149b005aee22fc29
kenenbek <kenenbek@gmail.com>
2023-12-11 05:28:27 +0600

    f

--- mymodels.py
+++ mymodels.py
@@ -164,7 +164,7 @@ class SimpleNN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         dp = 0.05
-        hidden_dim = 16
+        hidden_dim = 8
         self.model = Sequential(
             Linear(2635, hidden_dim),
             BatchNorm1d(hidden_dim),
