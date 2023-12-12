a1b6a42305d96832b75df3be449c1e3aa280081a
kenenbek <kenenbek@gmail.com>
2023-12-12 21:15:13 +0600

    f

--- mymodels.py
+++ mymodels.py
@@ -329,7 +329,7 @@ class Transformer(torch.nn.Module):
                                      concat=True,
                                      beta=False,
                                      dropout=0.0,
-                                     edge_dim=1,
+                                     edge_dim=0,
                                      bias=True,
                                      root_weight=True,
                                      )
@@ -341,7 +341,7 @@ class Transformer(torch.nn.Module):
 
     def forward(self, x_input, bf, sub_data_25, edge_index, edge_weight):
         res = []
-        h = self.conv1(bf, edge_index, edge_weight)
+        h = self.conv1(bf, edge_index)
         h = self.norm1(h)
         h = F.leaky_relu(h)
         res.append(h)
