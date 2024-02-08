7361e231f0a309a110ab5825341f0e2516d7b3b5
Kenenbek R <kenenbek@gmail.com>
2024-02-08 15:33:08 +0600

    d

--- run_10.py
+++ run_10.py
@@ -122,7 +122,7 @@ for k in range(10):
 
         out = model(big_features,
                     train_edge_index, train_edge_weight)
-        loss = criterion(out, full_data.y[train_mask])
+        loss = criterion(out[train_mask], full_data.y[train_mask])
 
         loss.backward()
         optimizer.step()
